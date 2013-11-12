ca6f6c429fe50773a4f44f3a4c8e0d251838f788
hemingang <netsharec@gmail.com>
2013-11-12 16:16:39 +0800

    add add_idimo_index()

--- innocent.c
+++ innocent.c
@@ -2,6 +2,7 @@
 #include <linux/types.h>
 #include <linux/list.h>
 #include <linux/jhash.h>
+#include <linux/slab.h>
 #include <linux/module.h>
 
 #define IDIMO_HASH_BITS 12
@@ -33,6 +34,26 @@ static struct idimo_index *get_idimo_index(const char *name)
 			return e;
 	}
 	return NULL;
+}
+
+static struct idimo_index *add_idimo_index(const char *name)
+{
+	struct hlist_head *head;
+	struct hlist_node *node;
+	struct idimo_index *e;
+	u32 hash = jhash(name, strlen(name), 0);
+
+	head = &idimo_table[hash & (IDIMO_TABLE_SIZE - 1)];
+	hlist_for_each_entry(e, node, head, hlist) {
+		if (!strcmp(name, e->idimo_index_name))
+			return ERR_PTR(-EEXIST); /* Already there */
+	}
+	e = kmalloc(sizeof(struct idimo_index), GFP_KERNEL);
+	if (!e)
+		return ERR_PTR(-ENOMEM);
+	memcpy(&e->idimo_index_name[0], name, 3);
+	hlist_add_head(&e->hlist, head);
+	return e;
 }
 
 static void init_idimo_data(void)
