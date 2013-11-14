f1dc2580feb8aec16fe27ffffdbe55e0ebc3011a
mingang.he <dustgle@gmail.com>
2013-11-14 20:06:37 +0800

    fix idimo_del_all_index()

--- innocent.c
+++ innocent.c
@@ -92,8 +92,8 @@ static void idimo_del_all_index(void)
 		hlist_for_each_safe(node, tmp, head) {
 			index = hlist_entry(node, struct idimo_index, hlist);
 			idimo_index_del_all_entry(index);
-			kfree(index);
 			hlist_del(node);
+			kfree(index);
 		}
 	}
 }
